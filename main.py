3c0a59bca8d195eb7f04e0e8d385965ff3480147
Edson Copque <ecop@disroot.org>
2025-03-04 18:51:34 -0300

    feat: new menu features added.

--- main.py
+++ main.py
@@ -1,38 +1,84 @@
 # FILE: /main.py
 
-saldo_conta = 0
-limite_saque = 500
-qtde_limite = 3
+updated_balance = 0
+limit_cashout = 500
+limit_qtde = 3
+
+cashouts = []
+deposits = []
 
 while True:
     try:
-        opcao = int(input('Choose one of the options:\n'
+        options = int(input('Choose one of the options:\n'
                                 '[1] - Cash out\n'
                                 '[2] - Deposit\n'
                                 '[3] - View extract\n'
                                 '[4] - Exit\n'
                                 '[5] - Info): '))
 
-        if opcao == 1:
-            valor_saque = float(input('Informe o valor de saque: '))
+        if options == 1:
+            cashout_amount = float(input('Enter the cash out amount: '))
 
-            if valor_saque > 0:
+            if cashout_amount > 0:
 
-                if valor_saque > limite_saque:
-                    print('Não é possível sacar valores acima de R$500,00.')
-                
-                elif qtde_limite == 0:
-                    print('Você atingiu o limite diário de saque.')
-
-                elif valor_saque > saldo_conta:
-                    print(f'Você não tem saldo em conta. Atual: R${saldo_conta:.2f}')
-                
+                if cashout_amount > limit_cashout:
+                    print('It is not possible to withdraw amounts above R$500.00.')
+                elif limit_qtde == 0:
+                    print('You have reached your daily withdrawal limit.')
+                elif cashout_amount > updated_balance:
+                    print(f'You do not have enough balance. Current: R${updated_balance:.2f}.')
                 else:
-                    qtde_limite -= 1
-                    saldo_conta -= valor_saque
-                    print(f'Saque: R${valor_saque:.2f}. Saldo: R${saldo_conta:.2f}')
+                    limit_qtde -= 1
+                    updated_balance -= cashout_amount
+                    cashouts.append(cashout_amount)
+                    print(f'Cash out: R${cashout_amount:.2f}. Balance: R${updated_balance:.2f}.')
             else:
-                print('Informe valores positivos.')
+                print('Enter only positive values.')
+
+        elif options == 2:
+            deposit_amount = float(input('Enter the deposit amount: '))
+
+            if deposit_amount > 0:
+                deposits.append(deposit_amount)
+                updated_balance += deposit_amount
+                print(f'Deposit: R${deposit_amount:.2f}. Updated balance: R${updated_balance}.')
+            else:
+                print('Enter only positive values.')
+
+        elif options == 3:
+            print('\n*** EXTRACT: ***')
+            
+            print('Deposits:')
+            if deposits:
+                for i1 in deposits:
+                    print(f'R${i1:.2f}')
+            else:
+                print('Nenhum depósito realizado.')
+            
+            print('Saques:')
+            if cashouts:
+                for i2 in cashouts:
+                    print(f'R${i2:.2f}')
+            else:
+                print('No deposit made.')
+
+        elif options == 5:
+            print('### DEVELOPER DATA ###')
+            print('Developed by: Edson Copque\n'
+                  'Site: https://linktr.ee/edsoncopque\n'
+                  'GitHub: https://github.com/ecopque\n'
+                  'Repo: https://github.com/ecopque/new_banking_system\n'
+                  'Signal Messenger: ecop.01\n')
+
+        elif options == 4:
+            print('Thank you for being our customer. Leaving...')
+            break
+        
+        else:
+            print('Please enter a valid number.')
 
     except ValueError:
-        print(f'Erro!')
+        print(f'Please enter a valid input.')
+
+
+# Edson Copque | https://linktr.ee/edsoncopque
