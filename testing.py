b5b48fad3607669c36ac2ecfec154a5e24f824c4
Edson Copque <ecop@disroot.org>
2025-03-15 15:56:22 -0300

    testing...

--- testing.py
+++ testing.py
@@ -1,2 +1,19 @@
 # FILE: /testing.py
 
+users = list()
+def create_users(name, birth, cpf: str, address: str):
+    for i1 in users:
+        if i1['cpf'] == cpf:
+            print('CPF already exists.')
+            break
+        
+        else:
+            new_user = {
+                'name': name,
+                'birth': birth,
+                'cpf': cpf,
+                'address': address
+            }
+
+            users.append(new_user)
+            print(f'User {name} created successfully.')
