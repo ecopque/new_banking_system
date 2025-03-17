160df4525524a40234f6a2bf61235636e6b9136f
Edson Copque <ecop@disroot.org>
2025-03-17 15:02:14 -0300

    testing...

--- testing.py
+++ testing.py
@@ -1,23 +1,24 @@
 # FILE: /testing.py
 
+#OK: 1. O programa deve armazenar os usuários em uma lista, um usuário é composto por: nome, data de nascimento, cpf e endereço. O endereço é uma string com o formato: logradouro, nro - bairro - cidade/sigla estado. Deve ser armazenado somente os números do CPF. Não podemos cadastrar 2 usuários com o mesmo CPF.
+
 users = list()
-def create_users(name, birth, cpf: str, address: str):
+def create_user(name, birth, cpf: str, address: str):
     cpf_exists = False
     for i1 in users:
         if i1['cpf'] == cpf:
+            print('User already exists.')
             cpf_exists = True
-            print('CPF already exists.')
             break
-    
+        
     if not cpf_exists:
-        new_users = {
+        new_user = {
             'name': name,
             'birth': birth,
             'cpf': cpf,
             'address': address
         }
-
-        users.append(new_users)
-        print(f'User {name} created.')
     
-...
+    users.append(new_user)
+    print(f'User {name} created.')
+    return
