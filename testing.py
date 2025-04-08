be54f1d8b8fb165dabdb9467875deab6224f6ee4
Edson Copque <ecop@disroot.org>
2025-04-08 20:42:44 -0300

    testing...

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@
 
 from abc import ABC, abstractmethod
 
-class Transaction:
+class Transaction(ABC):
     def __init__(self, value: float):
         self.value = value
 
@@ -11,6 +11,8 @@ class Transaction:
         pass
 
 class Deposit(Transaction):
+    
+    @abstractmethod
     def register(self, account):
         if self.value <= 0:
             print('Enter a positive number.')
@@ -18,14 +20,42 @@ class Deposit(Transaction):
         else:
             account.balance += self.value
             account.history.add_transaction(f'Deposit: {self.value:.2f}.')
-            return from django.utils.translation import ugettext_lazy as _
+            return True
 
 class Withdraw(Transaction):
+    
+    @abstractmethod
     def register(self, account):
         if self.value <= 0:
-            print(f'Enter a valid number.')
+            print('Enter a valid value.')
         elif self.value > account.balance:
-            print(f'You do not have enough money. {account.balance}.')
+            print('You do not have enough value.')
         else:
             account.balance -= self.value
-            account.history.add_transaction(self.value)
+            account.history.add_transaction(f'Withdraw: {self.value:.2f}.')
+            return True
+
+class History:
+    def __init__(self):
+        self.transactions = list()
+    
+    def add_transaction(self, description: str):
+        self.transactions.append(description)
+
+class Account:
+    def __init__(self, client, number, agency='0001', limit=500):
+        self.client = client
+        self.number = number
+        self.agency = agency
+        self.limit = limit
+
+        self.balance = 0
+        self.history = History()
+
+    def deposit(self, value):
+        transaction = Deposit(value):
+        return transaction.register(self)
+
+    def withdraw(self, value):
+        transaction = Withdraw(value):
+        return transaction.register(self)
