cc2f7ba1a6eaa64ef2ba68d554046e3a659d63c6
Edson Copque <ecop@disroot.org>
2025-04-14 14:57:57 -0300

    testing...

--- testing.py
+++ testing.py
@@ -1,15 +1,1 @@
 # FILE: /testing.py
-
-while True:
-    print('Who are you?')
-    name = str(input('What is your name?'))
-
-    if name != 'Joe':
-        continue
-
-    print('Hello Joe. What is the password?')
-    password = input()
-    if password == 'swordfish':
-        break
-
-print('Access garanted.')
