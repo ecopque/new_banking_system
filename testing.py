fe0d9b1c1394dad19e69129456e04e627cfa8925
Edson Copque <ecop@disroot.org>
2025-05-05 10:47:05 -0300

    Testing...

--- testing.py
+++ testing.py
@@ -1,1 +1,11 @@
 # FILE: /testing.py
+
+from abc import ABC, abstractclassmethod
+
+class A001_TransactionCLS(ABC):
+    def __init__(self, value: float):
+        self.A001_value = value
+
+    @abstractclassmethod
+    def A001_registerMTD(self, account):
+        pass
