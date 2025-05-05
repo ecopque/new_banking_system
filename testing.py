11498918827e10f619e97fa0c746618b72d1e311
Edson Copque <ecop@disroot.org>
2025-05-05 11:16:09 -0300

    Testing...

--- testing.py
+++ testing.py
@@ -20,3 +20,18 @@ class A002_DepositCLS(A001_TransactionCLS):
             account.C001_balance += self.A001_value
             account.C001_history.B001_add_transactionMTD(f'Deposit: {self.A001_value: .2f}.')
             return True
+
+class A003_WithdrawCLS(A001_TransactionCLS):
+    def A001_registerMTD(self, account):
+        if self.value <= 0:
+            print('Enter a positive value. ')
+            return False
+        
+        elif self.value > C001_balance:
+            print(f'You do not have enought balance: {self.account.C001_balance}.')
+            return False
+        
+        else:
+            account.C001_balance -= self.A001_value
+            account.C001_history.B001_add_transactionMTD(f'Withdra: {self.A001_value}.')
+            return True
